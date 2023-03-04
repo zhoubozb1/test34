1461f220fca405a8c79b9911997c01070ae3cf2f
root <1@qq.com>
2023-03-04 20:24:36 +0800

    11111

--- src/main/java/com/StartApplication.java
+++ src/main/java/com/StartApplication.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 public class StartApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("111111111111");
         System.out.println("mmm");
         System.out.println("dec");
         // Spring应用启动起来
